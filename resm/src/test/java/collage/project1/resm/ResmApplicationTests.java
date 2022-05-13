eb9dcc5b92c1941af47230f7d1859e39e433eff1
Hiba-mousli <hibamousli85@gmil.com>
2022-05-13 14:25:54 +0300

    --init

---
+++ resm/src/test/java/collage/project1/resm/ResmApplicationTests.java
@@ -0,0 +1,13 @@
+package collage.project1.resm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
